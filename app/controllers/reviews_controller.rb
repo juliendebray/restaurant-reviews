16a8cc010225b391ab292e322e09b8645d6e454b
Julien Debray <julien.debray.pro@gmail.com>
2014-11-13 16:04:23 +0100

    review controller fixed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,21 @@
 class ReviewsController < ApplicationController
-
   def create
-    @review = Review.new(review_params)
-    if @review.save
-      flash[:notice] = "Review #{@review.id} has been added"
-      redirect_to restaurant_path(@review.restaurant)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @new_review     = @restaurant.reviews.new(review_params)
+
+    if @new_review.save
+      flash[:notice] = "Review #{@new_review.id} has been added"
+      redirect_to restaurant_path(@restaurant)
     else
-      flash[:notice] = @review.errors.full_messages
-      redirect_to restaurant_path(@review.restaurant) #how to use render method?
+      flash.now[:alert] = @new_review.errors.full_messages
+      render 'restaurants/show'
+      # redirect_to restaurant_path(@review.restaurant) #how to use render method?
     end
   end
 
   private
-  def review_params
-    params.require(:review).permit(:content, :rating, :restaurant_id)
-  end
 
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112102044) do
+ActiveRecord::Schema.define(version: 20141113145314) do
+
+  create_table "cocktails", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ingredients", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
